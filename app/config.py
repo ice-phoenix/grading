718d402c9d1d4493920455f07940f4163c4092aa
George Pîrlea <george@dranov.ro>
2019-05-24 16:39:32 +0100

    Proper environment vars for deployment

--- app/config.py
+++ app/config.py
@@ -2,19 +2,21 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    ROOT_DIR = os.path.abspath(os.path.join(basedir, '../'))
+    PERSISTENT_DIR = os.environ.get('PERSISTENT_DIR') or os.path.join(ROOT_DIR, 'persistent')
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or '6cb49381bd6a6f239220e42'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(PERSISTENT_DIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    ROOT_DIR = os.path.abspath(os.path.join(basedir, '../'))
     CHECKER_PATH = os.path.join(ROOT_DIR, 'checker')
 
-    PROBLEM_DIR = os.path.join(ROOT_DIR, 'persistent/problems/')
-    TEAM_DIR = os.path.join(ROOT_DIR, 'persistent/teams/')
-
-    SUBMIT_DIR = os.path.join(ROOT_DIR, 'persistent/zips/')
+    PROBLEM_DIR = os.path.join(PERSISTENT_DIR, 'problems/')
+    TEAM_DIR = os.path.join(PERSISTENT_DIR, 'teams/')
+    SUBMIT_DIR = os.path.join(PERSISTENT_DIR, 'zips/')
     SUBMIT_DELAY = 10  # None or number of seconds
 
-    CELERY_BROKER_URL = 'pyamqp://guest@localhost//'
+    CELERY_BROKER_URL = os.environ.get('BROKER_URL') or 'pyamqp://guest@localhost//'
     CELERY_RESULT_BACKEND = 'rpc://'
+    CELERY_IGNORE_RESULT = True
