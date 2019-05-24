9d0fc67787bd3a1c6eb5adc49c7b88cb8a082d64
George Pîrlea <george@dranov.ro>
2019-05-24 21:53:51 +0100

    Gunicorn logging?

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from app import app
+import logging
 
 application=app
 
 if __name__ == "__main__":
     application.run()
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
